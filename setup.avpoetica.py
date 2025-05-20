57d753e6eb29063516dc5fc50d03fbe5a566428a
tph-kds <hungcompo123@gmail.com>
2025-05-20 16:57:17 +0700

    feat: upload the super structure code for the agent project

---
+++ setup.avpoetica.py
@@ -0,0 +1,9 @@
+import setuptools
+
+# Most of the package metadata (name, version, description, dependencies, etc.)
+# is defined in the pyproject.toml file.
+# setuptools will automatically read the configuration from pyproject.toml
+# when you run build or install commands (e.g., pip install .).
+
+if __name__ == "__main__":
+    setuptools.setup()
